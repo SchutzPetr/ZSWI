14f0889b4b018b23a89629427753811f895dce5b
ondrejvane <onry.vane@gmail.com>
2018-03-01 14:36:32 +0100

    Class for testing Easter Monday and Friday.

--- common/common-util/src/test/java/cz/zcu/attendance/util/EasterHolidayTest.java
+++ common/common-util/src/test/java/cz/zcu/attendance/util/EasterHolidayTest.java
@@ -22,4 +22,65 @@ class EasterHolidayTest {
   void getEasterFriday() {
     assertTrue(EasterHoliday.getEasterFriday(2018).isEqual(LocalDate.of(2018, 3, 30)));
   }
+
+  @Test
+  void getEasterMonday() {
+    assertTrue(EasterHoliday.getEasterMonday(2019).isEqual(LocalDate.of(2019, 4, 22)));
+  }
+
+  @Test
+  void getEasterFriday() {
+    assertTrue(EasterHoliday.getEasterFriday(2019).isEqual(LocalDate.of(2019, 4, 19)));
+  }
+
+  @Test
+  void getEasterMonday() {
+    assertTrue(EasterHoliday.getEasterMonday(2020).isEqual(LocalDate.of(2020, 4, 13)));
+  }
+
+  @Test
+  void getEasterFriday() {
+    assertTrue(EasterHoliday.getEasterFriday(2020).isEqual(LocalDate.of(2020, 4, 10)));
+  }
+
+  @Test
+  void getEasterMonday() {
+    assertTrue(EasterHoliday.getEasterMonday(2021).isEqual(LocalDate.of(2021, 4, 5)));
+  }
+
+  @Test
+  void getEasterFriday() {
+    assertTrue(EasterHoliday.getEasterFriday(2021).isEqual(LocalDate.of(2021, 4, 2)));
+  }
+
+  @Test
+  void getEasterMonday() {
+    assertTrue(EasterHoliday.getEasterMonday(2022).isEqual(LocalDate.of(2022, 4, 18)));
+  }
+
+  @Test
+  void getEasterFriday() {
+    assertTrue(EasterHoliday.getEasterFriday(2022).isEqual(LocalDate.of(2022, 4, 15)));
+  }
+
+  @Test
+  void getEasterMonday() {
+    assertTrue(EasterHoliday.getEasterMonday(2023).isEqual(LocalDate.of(2023, 4, 10)));
+  }
+
+  @Test
+  void getEasterFriday() {
+    assertTrue(EasterHoliday.getEasterFriday(2023).isEqual(LocalDate.of(2023, 4, 7)));
+  }
+
+  @Test
+  void getEasterMonday() {
+    assertTrue(EasterHoliday.getEasterMonday(2024).isEqual(LocalDate.of(2024, 4, 1)));
+  }
+
+  @Test
+  void getEasterFriday() {
+    assertTrue(EasterHoliday.getEasterFriday(2024).isEqual(LocalDate.of(2024, 3, 29)));
+  }
+
 }
