af2bd0d0179dc7a746831f74221a8e90d089b1e2
Petr Schütz <petr.schutz@unicorn.eu>
2018-03-01 10:08:57 +0100

    create @UtilityClass, code reformat

--- app/base-app/src/main/java/cz/zcu/attendance/EasterHoliday.java
+++ app/base-app/src/main/java/cz/zcu/attendance/EasterHoliday.java
@@ -1,63 +1,59 @@
 package cz.zcu.attendance;
 
 import java.time.LocalDate;
+import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.NotNull;
 
+@UtilityClass
 public class EasterHoliday {
 
-    int year;
 
-    private EasterHoliday(int year) {
-        this.year = year;
+  @NotNull
+  public LocalDate getEasterMonday(int year) {
+
+    int month;
+    int day;
+
+    int a = year % 19;
+    int b = year % 4;
+    int c = year % 7;
+
+    int m = 24;    //Constant for 20th and 21th century
+    int n = 5;    //Constant for 20th and 21th century
+
+    int d = (19 * a + m) % 30;
+    int e = (n + 2 * b + 4 * c + 6 * d) % 7;
+
+    int u = d + e - 9;
+
+    if (u == 25 && d == 28 && e == 6 && a > 10) {
+
+      month = 4;
+      day = 18;
+
+    } else if (u >= 1 && u <= 25) {
+
+      month = 4;
+      day = u;
+
+    } else if (u > 25) {
+
+      u = u - 7;
+      month = 4;
+      day = u;
+
+    } else {
+      u = 22 + d + e;
+      month = 3;
+      day = u;
     }
 
-    static LocalDate getEasterMonday(int year) {
+    return LocalDate.of(year, month, day).plusDays(1);
+  }
 
-        int month;
-        int day;
-
-        int a = year%19;
-        int b = year%4;
-        int c = year%7;
-
-        int m = 24;		//Constant for 20th and 21th century
-        int n = 5;		//Constant for 20th and 21th century
-
-        int d = (19*a + m)%30;
-        int e = (n + 2*b + 4*c + 6*d)%7;
-
-        int u = d + e - 9;
-
-        if(u==25 && d==28 && e==6 && a>10) {
-
-            month = 4;
-            day = 18;
-
-        }
-        else if(u>=1 && u<=25) {
-
-            month = 4;
-            day = u;
-
-        }
-        else if(u>25) {
-
-            u = u - 7;
-            month = 4;
-            day = u ;
-
-        }
-        else {
-            u = 22 + d + e;
-            month = 3;
-            day = u;
-        }
-
-        return LocalDate.of(year, month, day).plusDays(1);
-    }
-
-    static LocalDate getEasterFriday(int year) {
-
-        return getEasterMonday(year).minusDays(3);
-    }
+  @NotNull
+  static LocalDate getEasterFriday(int year) {
+    return getEasterMonday(year).minusDays(3);
+  }
 
 }
