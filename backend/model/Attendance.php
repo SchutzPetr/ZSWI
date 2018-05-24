a51184defb43d709a75d698844debd68d19c0254
ondrejvane <onry.vane@gmail.com>
2018-05-24 11:37:07 +0200

    Added class Attendance

---
+++ backend/model/Attendance.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ondrejvane
+ * Date: 24.05.18
+ * Time: 11:35
+ */
+
+class Attendance extends BaseModel
+{
+    private $activeFrom;
+    private $firstPartFrom;
+    private $firstPartTo;
+    private $secondPartFrom;
+    private $secondPartTo;
+}
