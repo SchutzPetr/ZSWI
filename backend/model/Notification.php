065bb394f1c260a93a34932a27b51020bac12442
ondrejvane <onry.vane@gmail.com>
2018-05-24 11:34:28 +0200

    Added class Notification

---
+++ backend/model/Notification.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ondrejvane
+ * Date: 24.05.18
+ * Time: 11:32
+ */
+
+class Notification extends BaseModel
+{
+    private $title;
+    private $description;
+    private $link;
+    private $read;
+}
