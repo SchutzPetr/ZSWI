148fed5dc9c568b7ebcc1e6c6a62cd6bd7fc5401
ondrejvane <onry.vane@gmail.com>
2018-05-24 11:30:19 +0200

    Added class UserContract.

---
+++ backend/model/UserContract.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ondrejvane
+ * Date: 24.05.18
+ * Time: 11:27
+ */
+
+class UserContract
+{
+    private $workStation;
+    private $obligation;
+    private $activeFrom;
+    private $activeTo;
+}
