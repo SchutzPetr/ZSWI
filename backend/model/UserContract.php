301afb5da00b14cf29de8363978c1d7d5f840fe9
ondrejvane <onry.vane@gmail.com>
2018-05-25 12:19:20 +0200

    Added some functions

--- backend/model/UserContract.php
+++ backend/model/UserContract.php
@@ -12,4 +12,70 @@ class UserContract extends BaseModel
     private $obligation = "";
     private $activeFrom = "";
     private $activeTo = "";
+
+    /**
+     * @return string
+     */
+    public function getWorkStation()
+    {
+        return $this->workStation;
+    }
+
+    /**
+     * @param string $workStation
+     */
+    public function setWorkStation($workStation)
+    {
+        $this->workStation = $workStation;
+    }
+
+    /**
+     * @return string
+     */
+    public function getObligation()
+    {
+        return $this->obligation;
+    }
+
+    /**
+     * @param string $obligation
+     */
+    public function setObligation($obligation)
+    {
+        $this->obligation = $obligation;
+    }
+
+    /**
+     * @return string
+     */
+    public function getActiveFrom()
+    {
+        return $this->activeFrom;
+    }
+
+    /**
+     * @param string $activeFrom
+     */
+    public function setActiveFrom($activeFrom)
+    {
+        $this->activeFrom = $activeFrom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getActiveTo()
+    {
+        return $this->activeTo;
+    }
+
+    /**
+     * @param string $activeTo
+     */
+    public function setActiveTo($activeTo)
+    {
+        $this->activeTo = $activeTo;
+    }
+
+
 }
