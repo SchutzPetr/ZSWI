7f267fe4ecb9a1b223125101be8bc497486564a5
ondrejvane <onry.vane@gmail.com>
2018-05-29 14:49:36 +0200

    All user holiday function ready and tested.

--- backend/model/UserHoliday.php
+++ backend/model/UserHoliday.php
@@ -6,10 +6,22 @@
  * Time: 23:17
  */
 
+include_once ('../database/Database.php');
+include_once ('BaseModel.php');
+
 class UserHoliday extends BaseModel
 {
-    private $userId = -1;   //TODO: Nastavit cizí klíč, pokud je zde -1 je porušena integrita databáze.
+    /**
+     * @var int
+     */
+    private $userId = -1;
+    /**
+     * @var string
+     */
     private $day = "";
+    /**
+     * @var string
+     */
     private $type = "";
 
     /**
@@ -66,18 +78,18 @@ class UserHoliday extends BaseModel
     private function fill($row)
     {
         self::setId($row["id"]);
-        //self::setUserId($row["user_id"]);
-        self::setDate($row["day"]);
+        self::setUserId($row["user_id"]);
+        self::setDay($row["day"]);
         self::setType($row["type"]);
     }
 
     /**
-     * @param $id
+     * @param int $id
      * @return UserHoliday
      */
     static function findById($id)
     {
-        $query = "SELECT * FROM user_holiday WHERE id = :id;";
+        $query = "SELECT * FROM user_vacation WHERE id = :id;";
         $preparedQuery = Database::getConnection()->prepare($query);
         $preparedQuery->bindValue(":id", $id);
         $preparedQuery->execute();
@@ -94,27 +106,28 @@ class UserHoliday extends BaseModel
      */
     static function findAll()
     {
-        $query = "SELECT * FROM user_holiday;";
+        $query = "SELECT * FROM user_vacation;";
         $preparedQuery = Database::getConnection()->prepare($query);
         $preparedQuery->execute();
         $result = $preparedQuery->fetchAll();
 
+        $arrayOfUserHoliday = array();
+
         foreach ($result as $var) {
             $instance = new self();
             $instance->fill($var);
-            $arrayOfUservacations[] = $instance;
+            $arrayOfUserHoliday[] = $instance;
 
         }
-        return $arrayOfUservacations;
+        return $arrayOfUserHoliday;
     }
 
     /**
-     * @param $holiday
-     *
+     * @param UserHoliday $userHoliday
      */
     static function save($userHoliday)
     {
-        $query = "insert into user_holiday (id, user_id, day, type) value (:id, :user_id, :day, :type) on duplicate key update user_id = :user_id, day = :day, type = :type;";
+        $query = "insert into user_vacation (id, user_id, day, type) value (:id, :user_id, :day, :type) on duplicate key update user_id = :user_id, day = :day, type = :type;";
         $preparedQuery = Database::getConnection()->prepare($query);
         $preparedQuery->bindValue(":id", $userHoliday->getId() == -1 ? null : $userHoliday->getId());
         $preparedQuery->bindValue(":user_id", $userHoliday->getUserId());
