f221371cb4a31874f9be7ac07f8a0f2e03c40865
Petr Schütz <petr.schutz@unicorn.com>
2018-06-15 17:48:38 +0200

    add ShareService structure

---
+++ backend/service/ShareService.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: schut
+ * Date: 14.06.2018
+ * Time: 19:14
+ */
+
+class ShareService
+{
+    /**
+     * @param integer $userId
+     */
+    public static function findAllSharedWithUserId($userId)
+    {
+
+    }
+
+    /**
+     * @param integer $userId
+     */
+    public static function findAllSharedWithOthers($userId)
+    {
+
+    }
+
+    /**
+     * @param integer $userId
+     */
+    public static function findAllAvailableUsers($userId)
+    {
+
+    }
+
+    /**
+     * @param integer $fromUserId
+     * @param integer[] $toUserIds
+     */
+    public static function createShare($fromUserId, $toUserIds){
+
+    }
+
+    /**
+     * @param integer $fromUserId
+     * @param integer $toUserId
+     */
+    public static function deleteShare($fromUserId, $toUserId){
+
+    }
+}
