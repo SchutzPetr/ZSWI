75dd8b9ecac84c2f007f4fb65222b580739413a3
ondrejvane <onry.vane@gmail.com>
2018-06-16 08:42:58 +0200

    #22 Service for share

--- backend/service/ShareService.php
+++ backend/service/ShareService.php
@@ -10,9 +10,16 @@ class ShareService
 {
     /**
      * @param integer $userId
+     * @return UserSharingTimesheet
+     * @throws PermissionException
      */
     public static function findAllSharedWithUserId($userId)
     {
+        if(!Permission::hasPermission(self::getUserFromContext(), "USER_SHARING_TIMESHEET.FIND")){
+            throw new PermissionException();
+        }
+
+        return UserSharingTimesheet::findAllSharedWithUserId($userId);
 
     }
 
@@ -35,16 +42,26 @@ class ShareService
     /**
      * @param integer $fromUserId
      * @param integer[] $toUserIds
+     * @throws PermissionException
      */
     public static function createShare($fromUserId, $toUserIds){
+        if(!Permission::hasPermission(self::getUserFromContext(), "USER_SHARING_TIMESHEET.CREATE")){
+            throw new PermissionException();
+        }
 
+        return UserSharingTimesheet::createShare($fromUserId, $toUserIds);
     }
 
     /**
      * @param integer $fromUserId
      * @param integer $toUserId
+     * @throws PermissionException
      */
     public static function deleteShare($fromUserId, $toUserId){
+        if(!Permission::hasPermission(self::getUserFromContext(), "USER_SHARING_TIMESHEET.DELETE")){
+            throw new PermissionException();
+        }
 
+        return UserSharingTimesheet::deleteShare($fromUserId, $toUserId);
     }
 }
