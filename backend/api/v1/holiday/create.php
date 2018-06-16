600e5a740fc54248b098b8664e55d2ac80dae8e0
ondrejvane <onry.vane@gmail.com>
2018-06-16 10:06:35 +0200

    #27 Added header to all controllers.

--- backend/api/v1/holiday/create.php
+++ backend/api/v1/holiday/create.php
@@ -9,6 +9,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: X-Auth-Token");
 
 include_once (__DIR__."/../FatalErrorHandler.php");
 include_once (__DIR__."/../../../service/HolidayService.php");
--- backend/api/v1/project/findById.php
+++ backend/api/v1/project/findById.php
@@ -10,6 +10,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: X-Auth-Token");
 
 include_once (__DIR__."/../FatalErrorHandler.php");
 include_once (__DIR__."/../../../service/ProjectService.php");
