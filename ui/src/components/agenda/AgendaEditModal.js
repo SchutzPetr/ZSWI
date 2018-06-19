bf51d504ff8d7893b3decbd3680dcebd293dc4c6
Petr Schütz <petr.schutz@unicorn.com>
2018-06-19 02:00:31 +0200

    close #45

--- ui/src/components/agenda/AgendaEditModal.js
+++ ui/src/components/agenda/AgendaEditModal.js
@@ -29,8 +29,6 @@ import Style from "./style/AgendaEditModalStyle";
 const dayTypes = {
     SICKNESS: "Nemoc",
     FAMILY_MEMBER_CARE: "OČR",
-    HOLIDAY: "Dovolená",
-    PUBLIC_HOLIDAY: "Státní svátek",
     BUSINESS_TRIP: "Služební cesta",
     WORK_OUTSIDE_WORKSPACE: "Práce mimo pracoviště",
 };
